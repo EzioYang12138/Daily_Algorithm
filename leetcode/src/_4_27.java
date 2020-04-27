35e398c2d41160c96e5b239c9e6022c9309c7375
jdkYang <940604949@qq.com>
2020-04-27 09:19:57 +0800

    4月27号leetcode 二分 搜索旋转排序数组

---
+++ leetcode/src/_4_27.java
@@ -0,0 +1,30 @@
+public class _4_27 {
+
+//    https://leetcode-cn.com/problems/search-in-rotated-sorted-array/
+
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
