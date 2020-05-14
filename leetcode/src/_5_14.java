4f859f7ad558893da7058b457251b8624c9acb69
jdkYang <940604949@qq.com>
2020-05-14 09:47:51 +0800

    5月14号 位运算

---
+++ leetcode/src/_5_14.java
@@ -0,0 +1,12 @@
+public class _5_14 {
+
+    public int singleNumber(int[] nums) {
+        int x = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            x ^= nums[i];
+        }
+
+        return x;
+    }
+}
