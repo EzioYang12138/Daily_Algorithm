fcf240f30ddad0f4c4f8e530fc294a4bb59d2506
jdkYang <940604949@qq.com>
2020-07-28 08:06:43 +0800

    递归

---
+++ leetcode/src/_7_28.java
@@ -0,0 +1,10 @@
+public class _7_28 {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
