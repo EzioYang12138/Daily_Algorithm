0677c8064b7dc37ff781dc1b8e952d08cfb32cdb
jdkYang <940604949@qq.com>
2021-03-06 12:00:52 +0800

    两个栈实现队列

--- leetcode_2021/src/_03_06.java
+++ leetcode_2021/src/_03_06.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Stack;
+
 public class _03_06 {
 
     public int[] nextGreaterElements(int[] nums) {
@@ -21,6 +24,29 @@ public class _03_06 {
         }
 
         return res;
+    }
+
+
+    public int[] nextGreaterElments(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+
+        Arrays.fill(res , -1);
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = 0; i < n * 2; i++) {
+            int num = nums[i % n];
+            while(!stack.isEmpty() && num > nums[stack.peek()]) {
+                res[stack.pop()] = num;
+            }
+
+            if(i < n){
+                stack.add(i);
+            }
+        }
+
+        return res;
+
     }
 
 
